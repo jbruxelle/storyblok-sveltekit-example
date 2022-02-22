8c9c42cd403a11ee649479237cfa7f7a40df677a
Julian BRUXELLE <bruxelle.julian@wanadoo.fr>
2022-02-22 09:28:44 +0100

    :fire: REMOVE: api folder

--- src/routes/api/playground.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-	export const load = async () => {
-		return {
-			status: 304,
-			redirect: 'https://gapi-browser.storyblok.com/?token=auS0kARoFKlmV1SQM4V5AAtt'
-		};
-	};
-</script>
