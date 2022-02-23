003afce41f9a4ae027e46fe080105d694eb481c2
Julian BRUXELLE <bruxelle.julian@wanadoo.fr>
2022-02-23 09:59:13 +0100

    :pencil2: FIX: resolver type after typename update

--- src/lib/utils/resolvers.ts
+++ src/lib/utils/resolvers.ts
@@ -1,7 +1,7 @@
-import type { LinkBlok } from '$lib/storyblok/component-types';
+import type { Link } from '$lib/storyblok/component-types';
 
 export const resolvers = {
-	link: (link: LinkBlok): string => {
+	link: (link: Link): string => {
 		if (!link) return '';
 		const base = '/';
 		const result = link.linktype === 'email' ? `mailto:${link.email}` : link.url || link.cached_url;
